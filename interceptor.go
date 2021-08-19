c0f8a211c88a436b3e2283197d94758b78c6fdec
mi2428 <mi2428782020@gmail.com>
2021-08-20 00:44:30 +0900

    Add extractor
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- interceptor.go
+++ interceptor.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
+	"os"
 	"time"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
 const (
-	device      string        = "eth1"        // Where DNS packets are forwarded
-	filter      string        = "dst port 53" // Only capturing DNS queries
+	device      string        = "eth1"                // Where DNS packets are forwarded
+	filter      string        = "udp and dst port 53" // Only capturing UDP DNS queries
 	snaplen     int32         = 1600
 	promiscuous bool          = true
 	timeout     time.Duration = pcap.BlockForever
@@ -22,6 +25,51 @@ var (
 	REVISION string = "develop"
 	err      error
 )
+
+type QueryLogItem struct {
+	timestamp time.Time
+	srcIP     net.IP
+	dstIP     net.IP
+	srcPort   uint16
+	query     string
+	rrType    string
+}
+
+func (q QueryLogItem) String() string {
+	return fmt.Sprintf("%s %s (%d) %s %s (%s)",
+		q.timestamp.String(), q.srcIP.String(), q.srcPort, q.dstIP.String(), q.query, q.rrType)
+}
+
+func newQueryLogItem(packet gopacket.Packet) *QueryLogItem {
+	q := new(QueryLogItem)
+	q.timestamp = packet.Metadata().Timestamp
+
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to decode some part of the packet: %v\n", err)
+		return q
+	}
+
+	if ip6Layer := packet.Layer(layers.LayerTypeIPv6); ip6Layer != nil {
+		ip6, _ := ip6Layer.(*layers.IPv6)
+		q.srcIP = ip6.SrcIP
+		q.dstIP = ip6.DstIP
+	}
+
+	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+		udp, _ := udpLayer.(*layers.UDP)
+		q.srcPort = uint16(udp.SrcPort)
+	}
+
+	if dnsLayer := packet.Layer(layers.LayerTypeDNS); dnsLayer != nil {
+		dns, _ := dnsLayer.(*layers.DNS)
+		for _, question := range dns.Questions {
+			q.query = string(question.Name)
+			q.rrType = question.Type.String()
+		}
+	}
+
+	return q
+}
 
 func main() {
 	fmt.Println("VERSION", VERSION+"-"+REVISION)
@@ -38,6 +86,7 @@ func main() {
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		fmt.Println(packet)
+		item := newQueryLogItem(packet)
+		fmt.Println(item.String())
 	}
 }
